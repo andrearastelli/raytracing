352f9404d684dd0af298099cf94f6e61ddaa66c1
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-04-16 07:48:08 +0100

    First image implementation and filesystem testing

--- src/image.h
+++ src/image.h
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <fstream>
+#include <filesystem>
 
 class Image
 {
+private:
+    std::string fileame;
 public:
-    float x;
+    Image() { std::cout << "Tmp dir: " << std::filesystem::tmp_directoy_path() << std::endl; }
+    Image(std::string filename): filename{filename} {}
+
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+#include "image.h"
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    Image i;
     return 0;
 }
