a21f53bfdcf13f6127632888ea821193507c9f28
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-04-17 08:15:19 +0100

    Updated Image documentation

--- src/image.h
+++ src/image.h
@@ -1,3 +1,11 @@
+/**
+ * Image.
+ *
+ * Class used for handling the image generation process into a file.
+ * The current implementation allows the creation of images in the PPM
+ * ASCII format.
+ */
+
 #include <iostream>
 #include <fstream>
 
@@ -16,13 +24,49 @@ private:
     int size_y;
 
 public:
+    /**
+     * Deleted default empty constructor.
+     */
     Image() = delete;
+
+    /**
+     * Filename and image size constructor.
+     *
+     * Will open the output image stream.
+     *
+     * @param filename Output image filename
+     * @param size_x Width of the image
+     * @param size_y Height of the image
+     */
     explicit Image(const std::string &filename, int size_x = 200, int size_y = 100);
+
+    /**
+     * Destructor.
+     *
+     * Will close the output image stream.
+     */
     ~Image();
 
+
+    /**
+     * Write into the image, appending the color information.
+     *
+     * @param color Color to add into the image.
+     */
     void write(const Color &color);
 
+    /**
+     * Returns the width of the image.
+     *
+     * @return Width of the image.
+     */
     inline int width() const { return size_x; }
+
+    /**
+     * Returns the height of the image.
+     *
+     * @return Height of the image.
+     */
     inline int height() const { return size_y; }
 };
 
