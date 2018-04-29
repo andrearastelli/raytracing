fa6c6efdd4e4b044d32b300fae620f89e498333a
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-04-29 10:49:00 +0100

    Updated camera with defocus.
    Added random_in_unit_disc in the camera.h header file.

--- src/camera.h
+++ src/camera.h
@@ -18,6 +18,9 @@
 #include "ray.h"
 
 
+Vec3 random_in_unit_disc();
+
+
 class Camera
 {
 
@@ -26,8 +29,11 @@ private:
     Vec3 lower_left_corner;
     Vec3 horizontal;
     Vec3 vertical;
+    Vec3 u, v, w;
+    float lens_radius;
 
 public:
+
     /**
      * Simple constructor that defines a custom oriented camera
      * using only vertical fov and aspect ratio.
@@ -36,6 +42,7 @@ public:
      * @param aspect Aspect ratio of the camera.
      */
     Camera(float vfov, float aspect);
+
 
     /**
      * Custom positionable camera with specific definition for
@@ -50,6 +57,20 @@ public:
      */
     Camera(Vec3 lookfrom, Vec3 lookat, Vec3 up, float vfov, float aspect);
 
+
+    /**
+     *
+     * @param lookfrom
+     * @param lookat
+     * @param up
+     * @param vfov
+     * @param aspect
+     * @param aperture
+     * @param focus_dist
+     */
+    Camera(Vec3 lookfrom, Vec3 lookat, Vec3 up, float vfov, float aspect, float aperture, float focus_dist);
+
+
     /**
      * Ray "emitted" from the camera into the world using the
      * (U, V) coordinate of the frame.
@@ -59,7 +80,7 @@ public:
      *
      * @return The Ray emitted from the camera into the world.
      */
-    Ray get_ray(float u, float v);
+    Ray get_ray(float s, float t);
 
 };
 
@@ -79,7 +100,6 @@ Camera::Camera(float vfov, float aspect)
 
 Camera::Camera(Vec3 lookfrom, Vec3 lookat, Vec3 up, float vfov, float aspect)
 {
-    Vec3 u, v, w;
     auto theta = static_cast<float>(vfov * M_PI / 180.0f);
     auto half_height = static_cast<float>(tan(theta / 2.0f));
     auto half_width = aspect * half_height;
@@ -96,9 +116,47 @@ Camera::Camera(Vec3 lookfrom, Vec3 lookat, Vec3 up, float vfov, float aspect)
 }
 
 
-Ray Camera::get_ray(float u, float v)
+Camera::Camera(Vec3 lookfrom, Vec3 lookat, Vec3 up, float vfov, float aspect, float aperture, float focus_dist)
 {
-    return {origin, lower_left_corner + u * horizontal + v * vertical - origin};
+    lens_radius = aperture / 2.0f;
+
+    auto theta = static_cast<float>(vfov * M_PI / 180.0f);
+    auto half_height = tan(theta / 2.0f);
+    auto half_width = aspect * half_height;
+
+    origin = lookfrom;
+
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(up, w));
+    v = cross(w, u);
+
+    lower_left_corner = origin - half_width * focus_dist * u - half_height * focus_dist * v - focus_dist * w;
+    horizontal = 2.0f * half_width * focus_dist * u;
+    vertical = 2.0f * half_height * focus_dist * v;
+}
+
+
+Ray Camera::get_ray(float s, float t)
+{
+    auto rd = lens_radius * random_in_unit_disc();
+    auto offset = u * rd.x() + v * rd.y();
+
+    return {
+        origin + offset,
+        lower_left_corner + s * horizontal + t * vertical - origin - offset
+    };
+}
+
+
+Vec3 random_in_unit_disc()
+{
+    Vec3 p;
+
+    do {
+        p = 2.0f * Vec3(drand48(), drand48(), drand48()) - Vec3(1.0f, 1.0f, 0.0f);
+    } while(dot(p, p) >= 1.0f);
+
+    return p;
 }
 
 
