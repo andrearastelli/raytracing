7e84138651ccb7594525736c26e6b8ec8453b534
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-04-29 10:28:17 +0100

    Updated samples evaluation to 16, for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,13 @@ Color ray_color(const Ray &r, Hitable *world, int depth);
 
 int main()
 {
+<<<<<<< HEAD
     Image image("test_metal.ppm");
+=======
+    Image image("test_camera.ppm");
+
+    auto samples = 16;
+>>>>>>> Updated samples evaluation to 16, for testing purposes
 
     auto samples = 8;
     
