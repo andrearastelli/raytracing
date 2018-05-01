a90661bbccb0ff77bab37153858c515b29f4fc32
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-05-01 07:44:49 +0100

    Parser header, defining all the parsing functionality for command line arguments.
    To store the parameter data a struct "Parameter" is required.
    Each attribute in this struct contain a single parameter data.

---
+++ src/parser.h
@@ -0,0 +1,51 @@
+#ifndef RAYTRACING_PARSER_H
+#define RAYTRACING_PARSER_H
+
+
+#include <string>
+
+
+struct Parameter
+{
+    int width = 200;
+    int height = 100;
+    int samples = 8;
+    std::string output_path = "temp.ppm";
+};
+
+
+Parameter parser(int argc, char *argv[])
+{
+    Parameter out_param;
+
+    for(std::size_t idx=0; idx<argc; ++idx)
+    {
+        std::string arg(argv[idx]);
+        std::size_t pos = 0;
+
+        while((pos = arg.find('=')) != std::string::npos)
+        {
+            auto param = arg.substr(0, pos);
+            auto value = arg.substr(pos+1, arg.length());
+
+            if (param == "--output")
+                out_param.output_path = value;
+
+            if (param == "--width")
+                out_param.width = std::stoi(value);
+
+            if (param == "--height")
+                out_param.height = std::stoi(value);
+
+            if (param == "--samples")
+                out_param.samples = std::stoi(value);
+
+            arg.erase(0, pos + 1);
+        }
+    }
+
+    return out_param;
+}
+
+
+#endif //RAYTRACING_PARSER_H
