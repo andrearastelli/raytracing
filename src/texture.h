bb6ea2d9c9952f3cbe98b0a8aca49b9299260494
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-05-24 07:48:53 +0100

    Color information instead of Vec3 returned

--- src/texture.h
+++ src/texture.h
@@ -125,7 +125,7 @@ public:
 
 Color NoiseTexture::value(float u, float v, const Vec3 &p) const
 {
-    return Vec3{1.0f, 1.0f, 1.0f} * noise.noise(scale * p);
+    return Color(1.0f, 1.0f, 1.0f) * noise.noise(scale * p);
 }
 
 #endif
