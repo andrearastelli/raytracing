89b02e06d6d1d3cdb6c1c8d84db96580be5c64f6
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-05-30 08:00:15 +0100

    Removed again constexpr - Visual Studio fix

--- src/hitable.h
+++ src/hitable.h
@@ -121,7 +121,7 @@ RotateY::RotateY(Hitable *p, float angle) : ptr(p)
     cos_theta = std::cos(radians);
     hasbox = ptr->bounding_box(0, 1, bbox);
 
-    constexpr float FLT_MAX = std::numeric_limits<float>::max();
+    float FLT_MAX = std::numeric_limits<float>::max();
 
     auto min = Vec3(FLT_MAX, FLT_MAX, FLT_MAX);
     auto max = Vec3(-FLT_MAX, -FLT_MAX, -FLT_MAX);
