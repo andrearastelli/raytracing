59cc9f36423fc7c6608ac5922e0c2f5a29435c20
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-06-10 01:07:26 +0100

    Updated main with all volume boxed

--- src/box.h
+++ src/box.h
@@ -38,13 +38,13 @@ Box::Box(const Vec3 &p0, const Vec3 &p1, Material *ptr)
     auto pyz = new YZ_Rect(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr);
 
     list[i++] =                 new XY_Rect(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr);
-    list[i++] = new FlipNormals(new XY_Rect(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr));
+    list[i++] = new FlipNormals(new XY_Rect(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), ptr));
 
     list[i++] =                 new XZ_Rect(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), ptr);
-    list[i++] = new FlipNormals(new XZ_Rect(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), ptr));
+    list[i++] = new FlipNormals(new XZ_Rect(p0.x(), p1.x(), p0.z(), p1.z(), p0.y(), ptr));
 
     list[i++] =                 new YZ_Rect(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr);
-    list[i++] = new FlipNormals(new YZ_Rect(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr));
+    list[i++] = new FlipNormals(new YZ_Rect(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), ptr));
 
     list_ptr = new HitableList(list, i);
 }
