ac06b5d9f7156abbb5c7fd79c34e455b706ce53c
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-07-10 08:20:25 +0100

    Complete Vec3 vectorization - Still working on retrieving values and setting cross product

--- src/vec3.h
+++ src/vec3.h
@@ -11,7 +11,7 @@
 class Vec3
 {
 
-private:
+public:
     //std::array<float, 3> v;
     __m128 v;
 
@@ -24,7 +24,9 @@ public:
 
 public:
     Vec3() { v = _mm_setzero_ps(); }
-    constexpr Vec3(float x, float y, float z, float w=0.0f) { v = _mm_set_ps(x, y, z, w); }
+    Vec3(float x, float y, float z, float w=0.0f) { v = _mm_set_ps(x, y, z, w); }
+    explicit Vec3(__m128 v) : v{v} {}
+
 
     // float x() const { return v[0]; }
     // float y() const { return v[1]; }
@@ -40,8 +42,8 @@ public:
 	Vec3& operator*=(const Vec3 &v1);
 	Vec3& operator/=(const Vec3 &v1);
 	
-	Vec3& operator*=(const float t);
-	Vec3& operator/=(const float t);
+	Vec3& operator*=(float t);
+	Vec3& operator/=(float t);
 	
 	float length() const 
 	{
@@ -58,91 +60,96 @@ public:
 };
 
 
-constexpr const Vec3 Vec3::X = { 1.0f, 0.0f, 0.0f };
-constexpr const Vec3 Vec3::Y = { 0.0f, 1.0f, 0.0f };
-constexpr const Vec3 Vec3::Z = { 0.0f, 0.0f, 1.0f };
-constexpr const Vec3 Vec3::ONE = { 1.0f, 1.0f, 1.0f };
-constexpr const Vec3 Vec3::ZERO = { 0.0f, 0.0f, 0.0f };
+// constexpr const Vec3 Vec3::X = { 1.0f, 0.0f, 0.0f };
+// constexpr const Vec3 Vec3::Y = { 0.0f, 1.0f, 0.0f };
+// constexpr const Vec3 Vec3::Z = { 0.0f, 0.0f, 1.0f };
+// constexpr const Vec3 Vec3::ONE = { 1.0f, 1.0f, 1.0f };
+// constexpr const Vec3 Vec3::ZERO = { 0.0f, 0.0f, 0.0f };
 
 
-std::ostream& operator<<(std::ostream &os, const Vec3 &t)
-{
-	os << t.x() << t.y() << t.z();
-	return os;
-}
+// std::ostream& operator<<(std::ostream &os, const Vec3 &t)
+// {
+// 	os << t.x() << t.y() << t.z();
+// 	return os;
+// }
 
 
 Vec3 operator+(const Vec3 &v1, const Vec3 &v2)
 {
-	return Vec3(v1.x()+v2.x(), v1.y()+v2.y(), v1.z()+v2.z());
+    auto res = _mm_add_ps(v1.v, v2.v);
+	return Vec3(res);
 }
 
 
 Vec3 operator-(const Vec3 &v1, const Vec3 &v2)
 {
-	return Vec3(v1.x()-v2.x(), v1.y()-v2.y(), v1.z()-v2.z());
+    auto res = _mm_sub_ps(v1.v, v2.v);
+	return Vec3(res);
 }
 
 
 Vec3 operator*(const Vec3 &v1, const Vec3 &v2)
 {
-	return Vec3(v1.x()*v2.x(), v1.y()*v2.y(), v1.z()*v2.z());
+    auto res = _mm_mul_ps(v1.v, v2.v);
+	return Vec3(res);
 }
 
 
 Vec3 operator/(const Vec3 &v1, const Vec3 &v2)
 {
-	return Vec3(v1.x()/v2.x(), v1.y()/v2.y(), v1.z()/v2.z());
+    auto res = _mm_div_ps(v1.v, v2.v);
+	return Vec3(res);
 }
 
 
-/**
- * Vec3 * float.
- *
- * @param v Vector value.
- * @param t Scalar value.
- *
- * @return vector * float result.
- */
 Vec3 operator*(const Vec3 &v, float t)
 {
-	return Vec3(t*v.x(), t*v.y(), t*v.z());
+    auto t_vec = _mm_set1_ps(t);
+    auto res = _mm_mul_ps(v.v, t_vec);
+	return Vec3(res);
 }
 
 
-/**
- * float * Vec3.
- *
- * @param t Scalar value.
- * @param v Vector value.
- *
- * @return float * vector result.
- */
 Vec3 operator*(float t, const Vec3 &v)
 {
-    return Vec3(t*v.x(), t*v.y(), t*v.z());
+    return v * t;
 }
 
 
 Vec3 operator/(const Vec3 &v, float t)
 {
-	return Vec3(v.x()/t, v.y()/t, v.z()/t);
+    auto t_vec = _mm_set1_ps(t);
+    auto res = _mm_div_ps(v.v, t_vec);
+	return Vec3(res);
 }
 
 
 float dot(const Vec3 &v1, const Vec3 &v2)
 {
-	return v1.x()*v2.x() + v1.y()*v2.y() + v1.z()*v2.z();
+    auto mul = v1 * v1;
+    auto res = _mm_cvtss_f32(mul.v);
+	return res;
 }
 
 
 Vec3 cross(const Vec3 &v1, const Vec3 &v2)
 {
-	return Vec3( 
-		(v1.y()*v2.z() - v1.z()*v2.y()),
-		(-(v1.x()*v2.z() - v1.z()*v2.x())),
-		(v1.x()*v2.y() - v1.y()*v2.x())
-	);
+    auto res = _mm_sub_ps(
+            _mm_mul_ps(                                                    // z, y, x, w
+                    _mm_shuffle_ps(v1.v, v1.v, _MM_SHUFFLE(3, 0, 2, 1)),   // w,
+                    _mm_shuffle_ps(v2.v, v2.v, _MM_SHUFFLE(3, 1, 0, 2))),
+            _mm_mul_ps(
+                    _mm_shuffle_ps(v1.v, v1.v, _MM_SHUFFLE(3, 1, 0, 2)),
+                    _mm_shuffle_ps(v2.v, v2.v, _MM_SHUFFLE(3, 0, 2, 1)))
+    );
+
+    // return Vec3(
+	// 	(v1.y()*v2.z() - v1.z()*v2.y()),
+	// 	(-(v1.x()*v2.z() - v1.z()*v2.x())),
+	// 	(v1.x()*v2.y() - v1.y()*v2.x())
+	// );
+
+    return Vec3(res);
 }
 
 
