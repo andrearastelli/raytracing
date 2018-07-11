661eb84968a1a56d40ffc82590b10e12a8a62ac1
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-07-11 08:01:31 +0100

    Updated all Vec3 to SIMD

--- src/vec3.h
+++ src/vec3.h
@@ -24,18 +24,17 @@ public:
 
 public:
     Vec3() { v = _mm_setzero_ps(); }
-    Vec3(float x, float y, float z, float w=0.0f) { v = _mm_set_ps(x, y, z, w); }
+    Vec3(float x, float y, float z, float w=0.0f) : v{_mm_set_ps(x, y, z, w)} {}
     explicit Vec3(__m128 v) : v{v} {}
 
-
-    // float x() const { return v[0]; }
-    // float y() const { return v[1]; }
-    // float z() const { return v[2]; }
+    float x() const { float v_f[4]{0}; _mm_store_ps(v_f, v); return v_f[0]; }
+    float y() const { float v_f[4]{0}; _mm_store_ps(v_f, v); return v_f[1]; }
+    float z() const { float v_f[4]{0}; _mm_store_ps(v_f, v); return v_f[2]; }
 	
-	// const Vec3& operator+() const { return *this; }
-	// Vec3 operator-() const { return Vec3(-v[0], -v[1], -v[2]); }
-	// float operator[](int i) const { return v[i]; }
-	// float& operator[](int i) { return v[i]; }
+	const Vec3& operator+() const { return *this; }
+	Vec3 operator-() const { return Vec3(_mm_mul_ps(v, _mm_set1_ps(-1))); }
+	float operator[](int i) const { float v_f[4]{0}; _mm_store_ps(v_f, v); return v_f[i]; }
+	float& operator[](int i) { float v_f[4]{0}; _mm_store_ps(v_f, v); return v_f[i]; }
 	
 	Vec3& operator+=(const Vec3 &v1);
 	Vec3& operator-=(const Vec3 &v1);
@@ -52,7 +51,10 @@ public:
 	
 	float squared_length() const 
 	{
-		return v[0]*v[0] + v[1]*v[1] + v[2]*v[2];
+	    float v_f[4]{0};
+	    auto res = _mm_mul_ps(v, v);
+	    _mm_store_ps(v_f, res);
+		return v_f[0] + v_f[1] + v_f[2];
 	}
 	
 	void normalize();
@@ -60,18 +62,18 @@ public:
 };
 
 
-// constexpr const Vec3 Vec3::X = { 1.0f, 0.0f, 0.0f };
-// constexpr const Vec3 Vec3::Y = { 0.0f, 1.0f, 0.0f };
-// constexpr const Vec3 Vec3::Z = { 0.0f, 0.0f, 1.0f };
-// constexpr const Vec3 Vec3::ONE = { 1.0f, 1.0f, 1.0f };
-// constexpr const Vec3 Vec3::ZERO = { 0.0f, 0.0f, 0.0f };
+const Vec3 Vec3::X = { 1.0f, 0.0f, 0.0f };
+const Vec3 Vec3::Y = { 0.0f, 1.0f, 0.0f };
+const Vec3 Vec3::Z = { 0.0f, 0.0f, 1.0f };
+const Vec3 Vec3::ONE = { 1.0f, 1.0f, 1.0f };
+const Vec3 Vec3::ZERO = { 0.0f, 0.0f, 0.0f };
 
 
-// std::ostream& operator<<(std::ostream &os, const Vec3 &t)
-// {
-// 	os << t.x() << t.y() << t.z();
-// 	return os;
-// }
+std::ostream& operator<<(std::ostream &os, const Vec3 &t)
+{
+	os << t.x() << t.y() << t.z();
+	return os;
+}
 
 
 Vec3 operator+(const Vec3 &v1, const Vec3 &v2)
@@ -135,19 +137,13 @@ float dot(const Vec3 &v1, const Vec3 &v2)
 Vec3 cross(const Vec3 &v1, const Vec3 &v2)
 {
     auto res = _mm_sub_ps(
-            _mm_mul_ps(                                                    // z, y, x, w
-                    _mm_shuffle_ps(v1.v, v1.v, _MM_SHUFFLE(3, 0, 2, 1)),   // w,
-                    _mm_shuffle_ps(v2.v, v2.v, _MM_SHUFFLE(3, 1, 0, 2))),
             _mm_mul_ps(
-                    _mm_shuffle_ps(v1.v, v1.v, _MM_SHUFFLE(3, 1, 0, 2)),
-                    _mm_shuffle_ps(v2.v, v2.v, _MM_SHUFFLE(3, 0, 2, 1)))
+                    _mm_shuffle_ps(v1.v, v1.v, _MM_SHUFFLE(1, 2, 0, 3)),
+                    _mm_shuffle_ps(v2.v, v2.v, _MM_SHUFFLE(2, 0, 1, 3))),
+            _mm_mul_ps(
+                    _mm_shuffle_ps(v1.v, v1.v, _MM_SHUFFLE(2, 0, 1, 3)),
+                    _mm_shuffle_ps(v2.v, v2.v, _MM_SHUFFLE(1, 2, 0, 3)))
     );
-
-    // return Vec3(
-	// 	(v1.y()*v2.z() - v1.z()*v2.y()),
-	// 	(-(v1.x()*v2.z() - v1.z()*v2.x())),
-	// 	(v1.x()*v2.y() - v1.y()*v2.x())
-	// );
 
     return Vec3(res);
 }
@@ -155,54 +151,42 @@ Vec3 cross(const Vec3 &v1, const Vec3 &v2)
 
 Vec3& Vec3::operator+=(const Vec3 &v1)
 {
-	v[0] += v1.v[0];
-	v[1] += v1.v[1];
-	v[2] += v1.v[2];
+    v = _mm_add_ps(v, v1.v);
 	return *this;
 }
 
 
 Vec3& Vec3::operator-=(const Vec3 &v1)
 {
-	v[0] -= v1.v[0];
-	v[1] -= v1.v[1];
-	v[2] -= v1.v[2];
+    v = _mm_sub_ps(v, v1.v);
 	return *this;
 }
 
 
 Vec3& Vec3::operator*=(const Vec3 &v1)
 {
-	v[0] *= v1.v[0];
-	v[1] *= v1.v[1];
-	v[2] *= v1.v[2];
+    v = _mm_mul_ps(v, v1.v);
 	return *this;
 }
 
 
 Vec3& Vec3::operator/=(const Vec3 &v1)
 {
-	v[0] /= v1.v[0];
-	v[1] /= v1.v[1];
-	v[2] /= v1.v[2];
+    v = _mm_div_ps(v, v1.v);
 	return *this;
 }
 
 
 Vec3& Vec3::operator*=(const float t)
 {
-	v[0] *= t;
-	v[1] *= t;
-	v[2] *= t;
+    v = _mm_mul_ps(v, _mm_set1_ps(t));
 	return *this;
 }
 
 
 Vec3& Vec3::operator/=(const float t)
 {
-	v[0] /= t;
-	v[1] /= t;
-	v[2] /= t;
+	v = _mm_div_ps(v, _mm_set1_ps(t));
 	return *this;
 }
 
@@ -210,9 +194,7 @@ Vec3& Vec3::operator/=(const float t)
 void Vec3::normalize()
 {
     float k = length();
-    v[0] /= k;
-    v[1] /= k;
-    v[2] /= k;
+    v = _mm_div_ps(v, _mm_set1_ps(k));
 }
 
 
