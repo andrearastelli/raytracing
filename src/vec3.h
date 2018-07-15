f43f682144007382c7b95a459a138b5dfb8d2169
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-07-15 18:21:54 +0100

    still need to figure out the dot product

--- src/vec3.h
+++ src/vec3.h
@@ -5,15 +5,15 @@
 #include <array>
 #include <cmath>
 
-#include <immintrin.h>
-
+#include <x86intrin.h>
 
 class Vec3
 {
 
 public:
     union {
-        float v_f[4] __attribute__((aligned(0x1000)));
+        //float v_f[4] __attribute__((aligned(0x1000)));
+        struct { float x, y, z; } __attribute__((aligned(0x1000)));
         __m128 v;
     };
 
@@ -25,21 +25,38 @@ public:
     static const Vec3 ZERO;
 
 public:
-    Vec3() : v_f{0} {}
-    Vec3(float x, float y, float z, float w=0.0f) : v_f{x, y, z, w} {}
+    Vec3() { _mm_setzero_ps(); }
+    Vec3(float x, float y, float z, float w=0.0f) { _mm_set_ps(w, z, y, x); }
     explicit Vec3(__m128 v) : v{v} {}
 
-    float x() const { float v_f[4]{0}; _mm_storeu_ps(v_f, v); return v_f[0]; }
-    float y() const { float v_f[4]{0}; _mm_storeu_ps(v_f, v); return v_f[1]; }
-    float z() const { float v_f[4]{0}; _mm_storeu_ps(v_f, v); return v_f[2]; }
+    //float x() const { float v_f[4]{0}; _mm_storeu_ps(v_f, v); return v_f[0]; }
+    //float y() const { float v_f[4]{0}; _mm_storeu_ps(v_f, v); return v_f[1]; }
+    //float z() const { float v_f[4]{0}; _mm_storeu_ps(v_f, v); return v_f[2]; }
 	
 	const Vec3& operator+() const { return *this; }
 	Vec3 operator-() const { return Vec3(_mm_mul_ps(v, _mm_set1_ps(-1))); }
 	float operator[](int i) const {
-        return v_f[i];
+        switch (i)
+        {
+            case 0: return x;
+            case 1: return y;
+            case 2: return z;
+        }
+        auto error_text = "Index [" + std::to_string(i) + "] value not allowed.";
+        throw std::runtime_error(error_text);
+        // return v_f[3-i];
     }
-	float& operator[](int i) {
-        return v_f[i]; }
+    float& operator[](int i) {
+        switch (i)
+        {
+            case 0: return x;
+            case 1: return y;
+            case 2: return z;
+        }
+        auto error_text = "Index [" + std::to_string(i) + "] value not allowed.";
+        throw std::runtime_error(error_text);
+        // return v_f[3-i];
+     }
 	
 	Vec3& operator+=(const Vec3 &v1);
 	Vec3& operator-=(const Vec3 &v1);
@@ -76,7 +93,8 @@ const Vec3 Vec3::ZERO = { 0.0f, 0.0f, 0.0f };
 
 std::ostream& operator<<(std::ostream &os, const Vec3 &t)
 {
-	os << "[" << t.x() << ", " << t.y() << ", " << t.z() << "]";
+	// os << "[" << t.x() << ", " << t.y() << ", " << t.z() << "]";
+    os << "[" << t.x << ", " << t.y << ", " << t.z << "]";
 	return os;
 }
 
@@ -134,8 +152,11 @@ Vec3 operator/(const Vec3 &v, float t)
 float dot(const Vec3 &v1, const Vec3 &v2)
 {
     auto mul = v1 * v2;
-    auto res = mul.v_f[0] + mul.v_f[1] + mul.v_f[2];
-	return res;
+    // auto res = mul.v_f[0] + mul.v_f[1] + mul.v_f[2];
+    // auto res = _mm_cvtss_f32(_mm_dp_ps(v1.v, v2.v, 0x71));
+    auto res = _mm_hadd_ps(mul.v, mul.v);
+    res = _mm_hadd_ps(res, res);
+	return res[0];
 }
 
 
