1747cf21b06a11bd34f7f15ec072ffdac1fda7e3
Andrea Rastelli <rastelliandrea87@gmail.com>
2018-07-23 07:08:55 +0100

    Updated SIMD check class

--- src/simd.h
+++ src/simd.h
@@ -15,43 +15,43 @@ void cpuid(int info[4], int InfoType) {
 struct CPUInfo
 {
 //  Misc.
-    bool HW_MMX;
-    bool HW_x64;
-    bool HW_ABM;      // Advanced Bit Manipulation
-    bool HW_RDRAND;
-    bool HW_BMI1;
-    bool HW_BMI2;
-    bool HW_ADX;
-    bool HW_PREFETCHWT1;
+    bool HW_MMX = false;
+    bool HW_x64 = false;
+    bool HW_ABM = false;      // Advanced Bit Manipulation
+    bool HW_RDRAND = false;
+    bool HW_BMI1 = false;
+    bool HW_BMI2 = false;
+    bool HW_ADX = false;
+    bool HW_PREFETCHWT1 = false;
 
 //  SIMD: 128-bit
-    bool HW_SSE;
-    bool HW_SSE2;
-    bool HW_SSE3;
-    bool HW_SSSE3;
-    bool HW_SSE41;
-    bool HW_SSE42;
-    bool HW_SSE4a;
-    bool HW_AES;
-    bool HW_SHA;
+    bool HW_SSE = false;
+    bool HW_SSE2 = false;
+    bool HW_SSE3 = false;
+    bool HW_SSSE3 = false;
+    bool HW_SSE41 = false;
+    bool HW_SSE42 = false;
+    bool HW_SSE4a = false;
+    bool HW_AES = false;
+    bool HW_SHA = false;
 
 //  SIMD: 256-bit
-    bool HW_AVX;
-    bool HW_XOP;
-    bool HW_FMA3;
-    bool HW_FMA4;
-    bool HW_AVX2;
+    bool HW_AVX = false;
+    bool HW_XOP = false;
+    bool HW_FMA3 = false;
+    bool HW_FMA4 = false;
+    bool HW_AVX2 = false;
 
 //  SIMD: 512-bit
-    bool HW_AVX512F;    //  AVX512 Foundation
-    bool HW_AVX512CD;   //  AVX512 Conflict Detection
-    bool HW_AVX512PF;   //  AVX512 Prefetch
-    bool HW_AVX512ER;   //  AVX512 Exponential + Reciprocal
-    bool HW_AVX512VL;   //  AVX512 Vector Length Extensions
-    bool HW_AVX512BW;   //  AVX512 Byte + Word
-    bool HW_AVX512DQ;   //  AVX512 Doubleword + Quadword
-    bool HW_AVX512IFMA; //  AVX512 Integer 52-bit Fused Multiply-Add
-    bool HW_AVX512VBMI; //  AVX512 Vector Byte Manipulation Instructions
+    bool HW_AVX512F = false;    //  AVX512 Foundation
+    bool HW_AVX512CD = false;   //  AVX512 Conflict Detection
+    bool HW_AVX512PF = false;   //  AVX512 Prefetch
+    bool HW_AVX512ER = false;   //  AVX512 Exponential + Reciprocal
+    bool HW_AVX512VL = false;   //  AVX512 Vector Length Extensions
+    bool HW_AVX512BW = false;   //  AVX512 Byte + Word
+    bool HW_AVX512DQ = false;   //  AVX512 Doubleword + Quadword
+    bool HW_AVX512IFMA = false; //  AVX512 Integer 52-bit Fused Multiply-Add
+    bool HW_AVX512VBMI = false; //  AVX512 Vector Byte Manipulation Instructions
 
     CPUInfo()
     {
@@ -109,6 +109,39 @@ struct CPUInfo
             HW_XOP   = (info[2] & ((int)1 << 11)) != 0;
         }
     }
+
+    bool is_mmx() { return HW_MMX; }
+    bool is_x64() { return HW_x64; }
+    bool is_abm() { return HW_ABM; }
+    bool is_rdrand() { return HW_RDRAND; }
+    bool is_bmi1() { return HW_BMI1; }
+    bool is_bmi2() { return HW_BMI2; }
+    bool is_adx() { return HW_ADX; }
+    bool is_prefetchwt1() { return HW_PREFETCHWT1; }
+    bool is_sse() { return HW_SSE; }
+    bool is_sse2() { return HW_SSE2; }
+    bool is_sse3() { return HW_SSE3; }
+    bool is_ssse3() { return HW_SSSE3; }
+    bool is_sse41() { return HW_SSE41; }
+    bool is_sse42() { return HW_SSE42; }
+    bool is_sse4a() { return HW_SSE4a; }
+    bool is_aes() { return HW_AES; }
+    bool is_sha() { return HW_SHA; }
+    bool is_avx() { return HW_AVX; }
+    bool is_xop() { return HW_XOP; }
+    bool is_fma3() { return HW_FMA3; }
+    bool is_fma4() { return HW_FMA4; }
+    bool is_avx2() { return HW_AVX2; }
+    bool is_avx512f() { return HW_AVX512F; }
+    bool is_avx512cd() { return HW_AVX512CD; }
+    bool is_avx512pf() { return HW_AVX512PF; }
+    bool is_avx512er() { return HW_AVX512ER; }
+    bool is_avx512vl() { return HW_AVX512VL; }
+    bool is_avx512bw() { return HW_AVX512BW; }
+    bool is_avx512dq() { return HW_AVX512DQ; }
+    bool is_avx512ifma() { return HW_AVX512IFMA; }
+    bool is_avx512vbmi() { return HW_AVX512VBMI; }
+
 };
 
 
